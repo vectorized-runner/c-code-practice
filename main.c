6b7b12f85b6c2ec418321eb8e66851c48aecfd92
Bartu Baş <bartubas99@gmail.com>
2025-06-11 10:24:57 +0300

    value init example, move some other stuff

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #include <stdint.h>
 #include <stdio.h>
 #include <tgmath.h>
+#include <_static_assert.h>
 
 // #1: Integer names
 uint8_t a;
@@ -24,12 +25,54 @@ void example_typedef() {
     my_data_typedef instance2;
 }
 
-// #5 auto
-void auto_example() {
-    auto myf = 123.0f;
-    auto myx = cos(myf);
+// #4 value initialization - simple
+typedef struct my_point {
+    int x;
+    int y;
+    int z;
+} my_point;
+
+my_point example_init_1() {
+    my_point p = {.x = 1, .y = 2, .z = 3 };
+    return p;
 }
+
+void example_init() {
+    struct { int a[3], b; } w[] = { [0].a = {1}, [1].a[0] = 2 };
+}
+
+/*
+// #6 constexpr
+int calculate_t() {
+    int m = 0;
+
+    for (int i = 0; i < 10; i++) {
+        m += i;
+    }
+
+    return m;
+}
+
+constexpr int t = calculate_t();
+*/
+
+// #5 static assert
+void static_assert_example() {
+    // static_assert(2 + 2 == 3, "you done fucked up");
+}
+
+
+// Stuff I couldn't make work
+// auto
+void auto_example() {
+    //auto myf = 123.0f;
+    //auto myx = cos(myf);
+}
+
 int main(void) {
+    // printf("%d\n", t);
+    auto m = 1;
+
     printf("aaa");
     return 0;
 }
