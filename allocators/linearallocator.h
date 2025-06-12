db63aa154c523d997196c2406beaa1142a361f01
Bartu Baş <bartubas99@gmail.com>
2025-06-12 17:30:18 +0300

    create uses init method

--- allocators/linearallocator.h
+++ allocators/linearallocator.h
@@ -6,26 +6,30 @@
 typedef struct linear_allocator {
     void* buffer;
     int size;
-    int current;
+    int offset;
 } linear_allocator;
 
-linear_allocator linear_allocator_create(int size) {
+void linear_allocator_init(linear_allocator* alloc, void* buffer, int size) {
     debug_assert(size > 0, "linear allocator: size must be positive");
+    alloc->buffer = buffer;
+    alloc->offset = 0;
+    alloc->size = size;
+}
+
+linear_allocator linear_allocator_create(int size) {
     linear_allocator alloc;
-    alloc.buffer = malloc(size);
-    alloc.size = size;
-    alloc.current = 0;
+    linear_allocator_init(&alloc, malloc(size), size);
     return alloc;
 }
 
 // TODO: Clear memory
 // TODO: Alignment
 void* linear_allocator_alloc(linear_allocator* alloc, int size) {
-    int prevCurrent = alloc->current;
-    int newCurrent = prevCurrent + size;
-    debug_assert(newCurrent <= alloc->size, "linear allocator: alloc out of bounds");
-    void* newPtr = alloc->buffer + prevCurrent;
-    alloc->current = newCurrent;
+    int prevOffset = alloc->offset;
+    int newOffset = prevOffset + size;
+    debug_assert(newOffset <= alloc->size, "linear allocator: alloc out of bounds");
+    void* newPtr = alloc->buffer + prevOffset;
+    alloc->offset = newOffset;
     return newPtr;
 }
 
