eca0377a7dc711ff178f2be4e3fafad41d59cdc5
Bartu Baş <bartubas99@gmail.com>
2025-06-12 11:50:49 +0300

    create new empty main

---
+++ main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+// todo: data structure
+// todo: bit hacking
+// todo: memcpy, memcmp
+// todo: ecs
+// todo: assertions
+// todo: compiler flags (-o3 or something)
+// todo: allocators
+// todo: unit testing
+// todo: which compiler am I compiling with?
+
+int main(void) {
+    printf("running main!");
+    return 0;
+}
